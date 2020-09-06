a0452f22ddc6bf6fd9cf5859d638667d4b7703d9
arda <arda@a.com>
2020-09-06 21:59:15 +0300

    products unit test imp.

--- product/src/test/java/com/tech/assignment/product/ProductsUnitTest.kt
+++ product/src/test/java/com/tech/assignment/product/ProductsUnitTest.kt
@@ -28,17 +28,19 @@ class ProductsUnitTest {
     @Test
     fun testGetProductDetailsSuccess() = runBlocking {
         productService = RetrofitHelper.getRetrofit(BASE_URL)
-        assert(productService.productDetails("1").isSuccessful)
+        assert(productService.productDetails(VALID_PRODUCT_ID).isSuccessful)
     }
 
     @Test
     fun testGetProductDetailsFailed() = runBlocking {
         productService = RetrofitHelper.getRetrofit(BASE_URL)
-        assert(productService.productDetails("-1").isSuccessful)
+        assert(productService.productDetails(INVALID_PRODUCT_ID).isSuccessful)
     }
 
     companion object {
         private const val BASE_URL = "https://s3-eu-west-1.amazonaws.com/"
         private const val FAKE_URL = "fake-url"
+        private const val VALID_PRODUCT_ID = "1"
+        private const val INVALID_PRODUCT_ID = "-1"
     }
 }
