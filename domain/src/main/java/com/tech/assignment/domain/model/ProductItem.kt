3f9d21da6240791a0be52724b78fdf2fa0ad620d
arda <arda@a.com>
2020-09-07 14:55:40 +0300

    unused class remove

--- domain/src/main/java/com/tech/assignment/domain/model/ProductItem.kt
+++ domain/src/main/java/com/tech/assignment/domain/model/ProductItem.kt
@@ -1,14 +1,11 @@
 package com.tech.assignment.domain.model
 
-import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
 
 @Entity(tableName = "ProductsTable")
-@Parcelize
 data class ProductItem(
     @PrimaryKey(autoGenerate = true) val id: Int,
     @SerializedName("product_id") var productId: String?,
@@ -16,6 +13,6 @@ data class ProductItem(
     @SerializedName("price") var price: Int?,
     @SerializedName("image") var image: String?,
     @SerializedName("description") var description: String?
-) : Parcelable
+)
 
 
